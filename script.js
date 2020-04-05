fc9d5bb2cfe87ad7b19a16d6b29b95c8e6e95fea
Tomay91 <tomay91@gmail.com>
2020-04-05 00:02:45 -0400

    homework update

--- script.js
+++ script.js
@@ -132,35 +132,4 @@ funciton questionClick() {
 }
 
 
-    window.onload=function() {
-        var startButton = document.getElementById("startBtn")
-        startButton.addEventListener("click", function(){
-            setTime();
-            setQuestion();
-            console.log(event);
-        });
-    }
-    var timeElement = document.querySelector("p");
-    var secondsLeft = 120;
-    function setTime() {
-        var timerInterval = setInterval(function() {
-            secondsLeft--;
-            timeElement.textContent = secondsLeft
-            if (secondsLeft === 0) {
-                clearInterval(timerInterval);
-                sendTimeMessage();
-            }
-        }, 1000);
-    }
-    function sendTimeMessage() {
-        timeElement.textContent = " ";
-    }
-    function setQuestion() {
-    }
-
-
-
-
-
-
-
+    
